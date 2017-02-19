c5991219862bb4e0dc6dd469c6f73d2ae80424c2
ace <andrew.emil@gmail.com>
2017-02-18 21:28:51 -0500

    Basic enemy generator

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -7,13 +7,28 @@ public class EnemyGenerator : MonoBehaviour {
 	public GameObject baseEnemy;
 	public GameObject player;
 	List<GameObject> currentEnemies;
+	private float lastPlacementTime;
 	// Use this for initialization
 	void Start () {
 		currentEnemies = new List<GameObject>();
+		lastPlacementTime = Time.realtimeSinceStartup;
+	}
+
+	void Update () {
+		if (ShouldPlaceNewEnemy ()) {
+			PlaceNewEnemy ();
+		}
+	}
+
+	private bool ShouldPlaceNewEnemy() {
+		if (Time.realtimeSinceStartup - lastPlacementTime > 10) {
+			lastPlacementTime = Time.realtimeSinceStartup;
+			return true;
+		}
+		return false;
 	}
 
 	public void PlaceNewEnemy() {
-		//TODO
 		GameObject newEnemy = Instantiate(baseEnemy);
 		EnemyController ec = newEnemy.GetComponent<EnemyController> ();
 		ec.Initialize (2, new Vector3 (10, 1, 10), .01f, player);
