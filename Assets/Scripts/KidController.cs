dc7c670ca945b54c2b3b2ee639480e4d4a0a50d8
ace <andrew.emil@gmail.com>
2017-03-11 18:32:31 -0500

    Support for kid moving through house

--- Assets/Scripts/KidController.cs
+++ Assets/Scripts/KidController.cs
@@ -1,15 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class KidController : MonoBehaviour {
 
+	IList<string> path;
+	GameObject currentWaypoint;
+	private float speed;
 	// Use this for initialization
 	void Start () {
-	
+		CreatePath ();
+		currentWaypoint = GameObject.Find ("StairWaypoint");
+		speed = 0.03f;
+	}
+
+	void CreatePath() {
+		path = new List<string> () {
+			"StairWaypoint", 
+			"PorchWaypoint", 
+			"FoyerWaypoint", 
+			"DiningRoomWaypoint", 
+			"KitchenWaypoint",
+			"KitchenHallwayWaypoint",
+			"MainHallwayWaypoint",
+			"MainHallwayEndWaypoint",
+			"BedroomWaypoint"
+		};
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Move ();
+	}
+
+	void Move ()
+	{
+		if (AtTarget ()) {
+			MaybeUpdateTarget ();
+		} else {
+			transform.position = transform.position + ((currentWaypoint.transform.position - transform.position).normalized * speed);
+		}
+	}
+
+	private bool AtTarget() {
+		return Vector3.Distance (transform.position, currentWaypoint.transform.position) < 1;
+	}
+
+	private void MaybeUpdateTarget() {
+		//TODO delay on moving to next target
+		if(ShouldUpdateTarget()) {
+			currentWaypoint = GameObject.Find (path [path.IndexOf (currentWaypoint.name) + 1]);
+		}
+	}
+
+	private bool ShouldUpdateTarget() {
+		if (InTerminalPosition ()) {
+			return false;
+		}
+		return true;
+	}
+
+	private bool InTerminalPosition() {
+		return currentWaypoint.name == "BedroomWaypoint";
 	}
 }
