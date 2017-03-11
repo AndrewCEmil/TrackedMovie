0652c01cdf356c56b6cb1058cdcf1576ab9fe82f
ace <andrew.emil@gmail.com>
2017-03-11 17:06:45 -0500

    Way more waypoints

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 public class PathFinder : MonoBehaviour {
 
 	IDictionary<string, IList<string>> waypointMap;
+	IDictionary<string, string> parentMap;
 	void Start () {
 		InitializeTree ();
 	}
@@ -43,28 +44,48 @@ public class PathFinder : MonoBehaviour {
 			tempFringe.Clear ();
 		}
 
-		return GameObject.Find ("StartWaypoint");
+		return GameObject.Find (parentMap [currentWaypoint]);
 	}
 
 	//Note that the tree is the inverse path because we find our way backwards
 	void InitializeTree() {
 		waypointMap = new Dictionary<string, IList<string>> ();
-		GameObject startWaypoint = GameObject.Find ("StartWaypoint");
-		GameObject stairWaypoint = GameObject.Find ("StairWaypoint");
-		GameObject porchWaypoint = GameObject.Find ("PorchWaypoint");
-		GameObject foyerWaypoint = GameObject.Find ("FoyerWaypoint");
-		GameObject KitchenWaypoint = GameObject.Find ("KitchenWaypoint");
+		parentMap = new Dictionary<string, string> ();
 
 		List<string> startChildren = new List<string> ();
 		List<string> stairChildren = new List<string> () { "StartWaypoint" };
 		List<string> porchChildren = new List<string> () { "StairWaypoint" };
 		List<string> foyerChildren = new List<string> () { "PorchWaypoint" };
-		List<string> kitchenChildren = new List<string> () { "FoyerWaypoint" };
+		List<string> diningRoomWaypoint = new List<string> () { "FoyerWaypoint" };
+		List<string> kitchenChildren = new List<string> () { "DiningRoomWaypoint" };
+		List<string> kitchenHallwayChildren = new List<string> () { "KitchenWaypoint" };
+		List<string> mainHallwayChildren = new List<string> () { "KitchenHallwayWaypoint" };
+		List<string> mainHallwayEndChildren = new List<string> () { "MainHallwayWaypoint" };
+		List<string> backdoorChildren = new List<string> () { "MainHallwayEndWaypoint" };
+		List<string> bedroomChildren = new List<string> () { "MainHallwayEndWaypoint" };
 
 		waypointMap.Add("StartWaypoint", startChildren);
 		waypointMap.Add("StairWaypoint", stairChildren);
 		waypointMap.Add("PorchWaypoint", porchChildren);
 		waypointMap.Add("FoyerWaypoint", foyerChildren);
+		waypointMap.Add("DiningRoomWaypoint", diningRoomWaypoint);
 		waypointMap.Add("KitchenWaypoint", kitchenChildren);
+		waypointMap.Add("KitchenHallwayWaypoint", kitchenHallwayChildren);
+		waypointMap.Add("MainHallwayWaypoint", mainHallwayChildren);
+		waypointMap.Add("MainHallwayEndWaypoint", mainHallwayEndChildren);
+		waypointMap.Add("BackdoorWaypoint", backdoorChildren);
+		waypointMap.Add("BedroomWaypoint", bedroomChildren);
+
+		parentMap.Add ("StartWaypoint", "StartWaypoint");
+		parentMap.Add ("StairWaypoint", "StartWaypoint");
+		parentMap.Add ("PorchWaypoint", "StairWaypoint");
+		parentMap.Add ("FoyerWaypoint", "PorchWaypoint");
+		parentMap.Add ("DiningRoomWaypoint", "FoyerWaypoint");
+		parentMap.Add ("KitchenWaypoint", "DiningRoomWaypoint");
+		parentMap.Add ("KitchenHallwayWaypoint", "KitchenWaypoint");
+		parentMap.Add ("MainHallwayWaypoint", "KitchenHallwayWaypoint");
+		parentMap.Add ("MainHallwayEndWaypoint", "MainHallwayWaypoint");
+		parentMap.Add ("BackdoorWaypoint", "MainHallwayEndWaypoint");
+		parentMap.Add ("BedroomWaypoint", "MainHallwayEndWaypoint");
 	}
 }
