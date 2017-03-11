e18e4ee33df56647875d30eba6b59a3f1fc8444a
ace <andrew.emil@gmail.com>
2017-03-11 12:58:52 -0500

    Got initial implementation of waypoints done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,19 +9,26 @@ public class PlayerController : MonoBehaviour {
 	private GameObject linkedTarget;
 	private EnemyGenerator enemyGenerator;
 	private AudioSource audioSource;
+	private GameObject currentWaypoint;
+	private bool inTransit;
+	private float speed;
 	void Start () {
 		Physics.gravity = new Vector3(0, -0.2F, 0);
 		GameObject levelObject = GameObject.Find ("LevelObject");
 		Physics.bounceThreshold = 0;
 		enemyGenerator = enemyGeneratorObj.GetComponent<EnemyGenerator> ();
 		audioSource = GetComponentInChildren<AudioSource> ();
+		inTransit = false;
+		speed = 0.1f;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		HandleMovement ();
 	}
 
 	void Shoot() {
+		//TODO dont shoot if we are selecting a waypoint
 		GameObject newBullet = Instantiate (bullet);
 		newBullet.SetActive (true);
 		newBullet.transform.position = transform.position + camera.transform.forward;
@@ -30,6 +37,27 @@ public class PlayerController : MonoBehaviour {
 		Vector3 realForward = camera.transform.forward;
 		bulletRB.AddForce (theForwardDirection * 200f);
 		PlayShootNoise ();
+	}
+
+	private void HandleMovement() {
+		if (InRangeOfWaypoint ()) {
+			inTransit = false;
+		} else {
+			DoMovement ();
+		}
+	}
+
+	private void DoMovement() {
+		transform.position = transform.position + ((currentWaypoint.transform.position - transform.position).normalized * speed);
+	}
+
+	private bool InRangeOfWaypoint() {
+		if (currentWaypoint == null) {
+			return true;
+		}
+
+		float dist = Vector3.Distance (gameObject.transform.position, currentWaypoint.transform.position);
+		return dist < 1.0;
 	}
 
 	public void BulletCollided(GameObject bulletObj) {
@@ -57,5 +85,19 @@ public class PlayerController : MonoBehaviour {
 	void TriggerPulled() {
 		Debug.Log("The trigger was pulled!");
 		Shoot ();
+	}
+
+	public void WaypointTriggered(GameObject waypoint) {
+		if (currentWaypoint == null || waypoint.GetInstanceID () != currentWaypoint.GetInstanceID()) {
+			HandleNewWaypoint (waypoint);
+		}
+	}
+
+	void HandleNewWaypoint(GameObject waypoint) {
+		if (currentWaypoint != null) {
+			currentWaypoint.GetComponent<WaypointController> ().CurrentlySelected (false);
+		}
+		currentWaypoint = waypoint;
+		currentWaypoint.GetComponent<WaypointController> ().CurrentlySelected (true);
 	}
 }
---
+++ Assets/Scripts/WaypointController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointController : MonoBehaviour, ICardboardGazeResponder {
+
+	private bool isGazedAt;
+	private bool isSelected;
+	private GameObject player;
+	private PlayerController playerController;
+	// Use this for initialization
+	void Start () {
+		isGazedAt = false;
+		isSelected = false;
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerController = player.GetComponent<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void SetGazedAt(bool isGazedAt) {
+		this.isGazedAt = isGazedAt;
+	}
+
+	private void WaypointTriggered() {
+		playerController.WaypointTriggered (gameObject);
+	}
+
+	public void CurrentlySelected(bool isSelected) {
+		this.isSelected = isSelected;
+		SetColor ();
+	}
+
+	private void SetColor() {
+		if (isSelected) {
+			TurnBlue ();
+		} else if (isGazedAt) {
+			TurnGreen ();
+		} else {
+			TurnRed ();
+		}
+	}
+
+	private void TurnBlue() {
+		GetComponent<Renderer> ().material.color = Color.blue;
+	}
+
+	private void TurnGreen() {
+		GetComponent<Renderer> ().material.color = Color.green;
+	}
+
+	private void TurnRed() {
+		GetComponent<Renderer> ().material.color = Color.red;
+	}
+
+
+	#region ICardboardGazeResponder implementation
+
+	/// Called when the user is looking on a GameObject with this script,
+	/// as long as it is set to an appropriate layer (see CardboardGaze).
+	public void OnGazeEnter() {
+		SetGazedAt(true);
+	}
+
+	/// Called when the user stops looking on the GameObject, after OnGazeEnter
+	/// was already called.
+	public void OnGazeExit() {
+		SetGazedAt(false);
+	}
+
+	// Called when the Cardboard trigger is used, between OnGazeEnter
+	/// and OnGazeExit.
+	public void OnGazeTrigger() {
+		WaypointTriggered ();
+	}
+
+	#endregion
+}
